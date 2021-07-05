e9295b7c01b657ad90a458daea0aafa2db1adecc
Krasimir Slavkov <muxozavar@yahoo.com>
2021-07-06 00:55:47 +0300

    Add zoom image

--- ti-broish/CustomControls/Controllers/ImagePreviewViewController.swift
+++ ti-broish/CustomControls/Controllers/ImagePreviewViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class ImagePreviewViewController: BaseViewController {
     
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var closeButton: UIButton!
     
@@ -22,16 +23,58 @@ final class ImagePreviewViewController: BaseViewController {
         loadImage()
     }
     
+    override func applyTheme() {
+        super.applyTheme()
+        scrollView.backgroundColor = view.backgroundColor
+    }
+    
     // MARK: - Private methods
     
     @IBAction private func handleCloseButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
+    @objc private func handlePinchGesture(_ sender: UIPinchGestureRecognizer) {
+        let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
+        
+        guard let scale = scaleResult, scale.a > 1.0, scale.d > 1.0 else {
+            return
+        }
+        
+        sender.view?.transform = scale
+        sender.scale = 1.0
+    }
+    
+    private func addPinchGesture() {
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGesture))
+        view.addGestureRecognizer(pinchGesture)
+    }
+    
     private func setupViews() {
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 10.0
+        
         closeButton.layer.cornerRadius = 4.0
         closeButton.backgroundColor = .primaryColor
         closeButton.setImage(UIImage(named: SharedAssetsConfig.close)?.withTintColor(.white), for: .normal)
+        
+        addPinchGesture()
+    }
+    
+    private func updateMinimumZoomScale() {
+        guard let image = imageView.image else {
+            return
+        }
+        
+        let imageWidth = CGFloat(view.bounds.size.width / image.size.width)
+        let imageHeight = CGFloat(view.bounds.size.height / image.size.height)
+        let minZoom: CGFloat = min(imageWidth, imageHeight)
+        
+        if (minZoom <= 1.0) {
+            scrollView!.minimumZoomScale = minZoom
+            scrollView!.zoomScale = minZoom
+        }
     }
     
     private func loadImage() {
@@ -40,6 +83,7 @@ final class ImagePreviewViewController: BaseViewController {
             return
         }
         
+        view.showLoading()
         DispatchQueue.global(qos: .background).async { [weak self] in
             guard let strongSelf = self else {
                 return
@@ -54,10 +98,23 @@ final class ImagePreviewViewController: BaseViewController {
                 
                 DispatchQueue.main.async {
                     strongSelf.imageView.image = UIImage(data: data)
+                    strongSelf.scrollView.contentSize = strongSelf.imageView.image?.size ?? .zero
+                    strongSelf.updateMinimumZoomScale()
+                    strongSelf.view.hideLoading()
                 }
             } catch {
                 print(error)
+                strongSelf.view.hideLoading()
             }
         }
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension ImagePreviewViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
